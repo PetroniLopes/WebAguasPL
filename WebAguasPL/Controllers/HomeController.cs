55b8fbc8626dda611a86d9d9afde99653fbc6de9
Pedro Lopes <Pedro.Lopes.24287@formandos.cinel.pt>
2022-11-27 03:31:34 +0000

    ContactUs

--- WebAguasPL/Controllers/HomeController.cs
+++ WebAguasPL/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Vereyon.Web;
+using WebAguasPL.Helpers;
 using WebAguasPL.Models;
 
 namespace WebAguasPL.Controllers
@@ -12,10 +16,16 @@ namespace WebAguasPL.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IFlashMessage _flashMessage;
+        private readonly IMailHelper _mailHelper;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IFlashMessage flashMessage, IMailHelper mailHelper, IConfiguration configuration)
         {
             _logger = logger;
+            _flashMessage = flashMessage;
+            _mailHelper = mailHelper;
+            _configuration = configuration;
         }
 
         
@@ -34,6 +44,47 @@ namespace WebAguasPL.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactUs(ContactUsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var mailTo = _configuration["Mail:From"];
+
+                if(mailTo == null)
+                {
+                    _flashMessage.Danger("Invalid Submission");
+                    return View(model);
+                }
+
+                Response response = _mailHelper.SendEmail(
+                    mailTo,
+                    model.Subject,
+
+                    $"<h1>Name: {model.Name}    Email: {model.Email}</h1>" + model.Message);
+
+                if (response.IsSuccess)
+                {
+                    _flashMessage.Confirmation("Submission sent");
+                    return View(model);
+
+                    //ViewBag.Message = "The instructions to allow your access has been sent to email";
+                    //return View();
+
+                }
+
+                _flashMessage.Danger("Invalid Submission");
+                return View(model);
+            }
+            _flashMessage.Danger("Invalid Submission");
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
